04df4109f856d1f137145a0bc88eeef9fbfd7d80
JNietoR <jorge.nieto35@gmail.com>
2023-10-21 00:55:47 +0200

    vista post y listado para imprimir los posts en tiempo real

--- Laravel/prueba/resources/views/posts/index.blade.php
+++ Laravel/prueba/resources/views/posts/index.blade.php
@@ -9,22 +9,14 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                <x-session-status class="mb-4" :status="session('status')" />    
-                <x-input-error :messages="$errors->get('body')" class="mt-2" />
+                
+                @include('posts.inc.form')
 
-                    <form action="{{ route('posts.store') }}" method="POST">
-                        @csrf
+                </div>
 
-                        <div class="mt-4">
-                            <x-input-label for="body" :value="__('Body')" />
-                            <x-textarea class="block mt-1 w-full" name="body" required/>
-                        </div>
-                        <div class="flex justify-end mt-4">
-                            <x-primary-button>
-                                {{ __('Save') }}
-                            </x-primary-button>
-                        </div>
-                    </form>
+                <div class="p-6">
+
+                @include('posts.inc.list')
 
                 </div>
             </div>
